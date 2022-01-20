7b12ebe86f85c5b5eab851da94a95511d8835170
Danyal Saqib <dsaqib.bee17seecs@seecs.edu.pk>
2022-01-20 11:31:46 +0500

    Update trt_inference.py

--- face_recognition/trt_inference.py
+++ face_recognition/trt_inference.py
@@ -109,7 +109,7 @@ def requestGenerator(batched_image_data, input_name, output_name, dtype, model_n
     # Set the input data
     inputs = []
     for in_layer in input_name:
-        inputs.append(client.InferRequestedOutput(in_layer))
+        inputs.append(client.InferInput(in_layer))
     #print("Datatype: ", dtype)
     #print("Batch Datatype: ", batched_image_data.dtype)
     #inputs[0].set_data_from_numpy(batched_image_data)
