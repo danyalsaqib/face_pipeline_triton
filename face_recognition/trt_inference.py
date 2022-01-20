0481275ea92d5b27d0479a0c46714f42204fc785
Danyal Saqib <dsaqib.bee17seecs@seecs.edu.pk>
2022-01-20 11:37:08 +0500

    Update trt_inference.py

--- face_recognition/trt_inference.py
+++ face_recognition/trt_inference.py
@@ -126,7 +126,7 @@ def convert_http_metadata_config(_metadata, _config):
 
     return _model_metadata, _model_config
 
-def trt_infer(blob, model_name, model_version=''):
+def trt_infer_original(blob, model_name, model_version=''):
 
     # Recognition
     #result = session.run([output_name], {input_name: blob})
@@ -195,6 +195,100 @@ def trt_infer(blob, model_name, model_version=''):
                 request_id=str(sent_count),
                 model_version=model_version,
                 outputs=outputs))
+    for async_request in async_requests:
+        responses.append(async_request.get_result())
+
+    #print("Responses Length: ", len(responses(output_name)))
+
+    output_array = []
+    for out_layer in output_name:
+        for response in responses:
+            this_id = response.get_response()["id"]
+            #print("Request {}, batch size {}".format(this_id, batch_size))
+            output_single = response.as_numpy(out_layer)
+            #print("Output Array: ", output_array)
+            #print("Output Array Shape: ", output_single.shape)
+            #postprocess(response, output_name, batch_size, max_batch_size > 0)
+            output_single = output_single.tolist()
+            output_array.append(output_single)
+
+    #print("Inference Successful")
+    #print("Responses: ", responses)
+    #print("Async Requests: ", async_requests)
+    #print("Output List Length: ", len(output_array))
+    return output_array
+
+
+def trt_infer(blob, model_name, model_version=''):
+
+    # Recognition
+    #result = session.run([output_name], {input_name: blob})
+    triton_client = httpclient.InferenceServerClient(
+                    'localhost:8003', verbose=False, concurrency=1)
+    batch_size = len(blob)
+    #print("Batch Size: ", batch_size)
+    responses = []
+    async_requests = []
+
+    #model_name = 'densenet_onnx'
+    #model_version = ''
+    #classes = 512
+
+    model_metadata = triton_client.get_model_metadata(
+        model_name=model_name, model_version=model_version)
+
+    model_config = triton_client.get_model_config(
+        model_name=model_name, model_version=model_version)
+    model_metadata, model_config = convert_http_metadata_config(
+        model_metadata, model_config)
+
+    max_batch_size, input_name, output_name, dtype = parse_model_2(
+        model_metadata, model_config)
+
+    #print("Output Names: ", output_name)
+    #print("Data Type: ", dtype)
+
+
+    image_idx = 0
+    #input_filenames=[]
+    repeated_image_data = []
+    #filenames = []
+    npdtype = triton_to_np_dtype(dtype)
+    blob = blob.astype(npdtype)
+
+    """
+    for i in batch_dict:
+        print("Filnames: ", batch_dict[i])
+        filenames.append(batch_dict[i])
+    """
+
+    for idx in range(batch_size):
+        #input_filenames.append(filenames[image_idx])
+        #print("Image_IDX: ", image_idx)
+        #print("image_data value: ", image_data[0])
+        repeated_image_data.append(blob[image_idx])
+        image_idx = (image_idx + 1) % len(blob)
+
+    if max_batch_size > 0:
+        batched_image_data = np.stack(repeated_image_data, axis=0)
+    else:
+        batched_image_data = repeated_image_data[0]
+
+    #print("Batched Image Data: ", batched_image_data)
+
+    sent_count = 0
+
+    inputs, outputs, model_name, model_version = requestGenerator(
+        batched_image_data, input_name, output_name, dtype, model_name, model_version):
+    for output in outputs:
+        sent_count += 1
+        async_requests.append(
+            triton_client.async_infer(
+                model_name,
+                inputs,
+                request_id=str(sent_count),
+                model_version=model_version,
+                outputs=output))
     for async_request in async_requests:
         responses.append(async_request.get_result())
 
